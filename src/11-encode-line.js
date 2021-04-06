81beb7ac7c738fce7d60896f8220ca6250bfecca
PrianiKate <li.liliana@yandex.ru>
2021-04-06 15:10:48 +0400

    feat: add task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let ans = '';
+  let i = 0;
+  while (i < str.length) {
+    let cnt = 1;
+    let j = i + 1;
+    while (j < str.length && str[i] === str[j]) {
+      j++;
+      cnt++;
+    }
+    if (cnt > 1) {
+      ans += cnt.toString();
+    }
+    ans += str[i];
+    i = j;
+  }
+  return ans;
 }
 
 module.exports = encodeLine;
