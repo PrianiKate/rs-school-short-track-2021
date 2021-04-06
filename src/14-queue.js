456a2b30a4b2153ca46ad4b240f02b7729b2652c
PrianiKate <li.liliana@yandex.ru>
2021-04-06 16:09:52 +0400

    feat: add task 14

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,4 +1,4 @@
-// const ListNode = require('../extensions/list-node');
+const ListNode = require('../extensions/list-node');
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
@@ -12,16 +12,26 @@
  */
 
 class Queue {
-  get size() {
-    throw new Error('Not implemented');
+  constructor() {
+    this.data = [];
+    this.top = 0;
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  get size() {
+    return this.top;
+  }
+
+  enqueue(element) {
+    const node = new ListNode(element);
+    console.log(node);
+    this.data.push(element);
+    this.top++;
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    const node = this.data.shift();
+    this.top--;
+    return node;
   }
 }
 
